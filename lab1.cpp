a8cb612919b7a33f8fbc5b3af78108517a3edc23
Dorafevr <daria.cojuhari@isa.utm.md>
2024-09-25 00:22:00 +0300

    add file lab1

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+#include  <iostream>
+int main(){
+
+int i=69;
+int number=72;
+i+=number;
+int a=15;
+arr[2]
+int* iPointer=&i;
+int* ter=&a;
+number=*iPointer; 
+ter=&i;/ 
+
+//std::cin>> a;
+//std::cin>>b;
+ 
+std::cout<<c<<std::endl;
+std::cout<<number<<std::endl;
+	 
+}
