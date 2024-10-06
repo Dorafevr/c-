2dfa0fbc80603abb6e35beb3bacffa7c37f038a8
Dorafevr <daria.cojuhari@isa.utm.md>
2024-10-06 23:13:24 +0300

    add

---
+++ lab5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    std::string_view h = "000011110f";
+    int coint = 0;
+    int first = 0;
+    int other = 0;
+    for (char c : h) {
+        if (c == '1') {
+            coint++;
+
+        }
+        else if (c == '0') {
+            first++;
+        }
+        else {
+            other++;
+        }
+    }
+   /*for (int i = 0; i < h.size(); i++) {
+        char c = h[i];  
+        if (c == '1') {   
+            coint++;
+        }
+    }*/ 
+    
+    std::cout << coint << std::endl;
+    std::cout << first << std::endl;
+    std::cout << other << std::endl;
+
+}
