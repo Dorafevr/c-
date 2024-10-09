81632995e43ed6431a17a017ee8b0cbb8ac626c7
Dorafevr <daria.cojuhari@isa.utm.md>
2024-10-09 09:50:00 +0300

    added

--- lab5.cpp
+++ lab5.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <cassert>
-
-int main() {
-    std::string_view h = "000011110f";
+int app(std::string_view h) {
     int coint = 0;
     int first = 0;
     int other = 0;
-    for (char c : h) {
+    /*for (char c : h) {
+         if (c == '1') {
+             coint++;
+
+         }
+         else if (c == '0') {
+             first++;
+         }
+         else {
+             other++;
+         }
+     }*/
+    for (int i = 0; i < h.size(); i++) {
+        char c = h[i];
+
         if (c == '1') {
             coint++;
-
         }
         else if (c == '0') {
             first++;
@@ -18,18 +29,30 @@ int main() {
             other++;
         }
     }
-   /*for (int i = 0; i < h.size(); i++) {
-        char c = h[i];  
-        if (c == '1') {   
-            coint++;
-        }
-    }*/ 
-    
     std::cout << coint << std::endl;
     std::cout << first << std::endl;
     std::cout << other << std::endl;
+    return coint;
+}
+void runTests()
+{
+    assert(app("000000") == 0);
+    assert(app("001100") == 2);
+    assert(app("111111") == 6);
+    assert(app("010011") == 3);
+    std::cout << "check";
+
+}
 
 
+int main() {
+
+    std::string_view h = "001011";
+    app(h);
+    runTests();
+
+
+    return 0;
 
 
 
